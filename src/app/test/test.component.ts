9d13b69ae5ee505a3eb52035b8320c362277f496
chenboyuan <591221298@qq.com>
2020-08-17 17:26:05 +0800

    first commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.less']
+})
+export class TestComponent implements OnInit {
+
+  oracle = 'wind';
+
+  constructor() { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+  }
+
+}
