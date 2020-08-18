213ced2b466c3eafa1acd36fe1d46e4d5e459d9b
chenboyuan <591221298@qq.com>
2020-08-18 10:15:46 +0800

    gogogo

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,10 +9,15 @@ export class TestComponent implements OnInit {
 
   oracle = 'wind';
   constructor() { }
-  
+  content = '<span>巴拉拉小魔仙</span>';
+  id = '123';
+  title = 'hello angular';
+  list = [{name: 'Jani' , country: 'Norway'},
+          {name: 'aplha', country: 'china'},
+          {name: 'bate' , country: 'america'}, ];
+  arrs = [1, 2 , 3, 4, 5];
   // tslint:disable-next-line: typedef
-  ngOnInit() {
-
+  ngOnInit() {// 生命周期钩子函数
 
   }
 
